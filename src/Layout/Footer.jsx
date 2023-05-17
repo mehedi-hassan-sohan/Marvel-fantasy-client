c577992983ba5eb56a94658b9c7bc42d2ae5ec96
mehedi-hassan-sohan <mehedihassansohan80@gmail.com>
2023-05-18 01:43:13 +0600

    footer   compeleted

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
   </div> 
   <div>
      <span className="footer-title">Company</span> 
-    <a className="link link-hover">About us</a> 
+      <a className="link link-hover">About us</a> 
     <a className="link link-hover">Contact</a> 
     <a className="link link-hover">Jobs</a> 
     <a className="link link-hover">Press kit</a>
