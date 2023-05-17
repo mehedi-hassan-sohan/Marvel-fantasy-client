752644f2f492266566fb004df06b97399e2f8aeb
mehedi-hassan-sohan <mehedihassansohan80@gmail.com>
2023-05-18 01:42:24 +0600

    navbar compeleted

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <span className="footer-title">Services</span> 
     <a className="link link-hover">Branding</a> 
      <a className="link link-hover">Design</a> 
-    <a className="link link-hover">Marketing</a> 
+     <a className="link link-hover">Marketing</a> 
      <a className="link link-hover">Advertisement</a>
   </div> 
   <div>
